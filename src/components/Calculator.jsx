53bb5cedbb1535496253fd402c385bf4b3c079a8
Alessio Manfredini <alessio.manfredini.work@gmail.com>
2024-04-30 15:49:32 +0200

    :hammer: Add operator handle function

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -5,33 +5,45 @@ import { useState } from 'react';
 function Calculator() {
     const [displayText, setDisplayText] = useState('');
     const handleDigitClick = (event) => setDisplayText(displayText + event.target.getAttribute('value'));
+    const handleOperatorClick = (event) => {
+        const lastChar = displayText[displayText.length - 1];
+        const newChar = event.target.getAttribute('value');
+
+        if (newChar != lastChar)
+            if (['x', '/', '+', '-'].includes(lastChar))
+                setDisplayText(displayText.slice(0, -1) + newChar);
+            else if (displayText != '')
+                handleDigitClick(event);
+    }
     const buttons = [
-        { key: 0, value: '+', fun: () => { } },
-        { key: 1, value: '-', fun: () => { } },
-        { key: 2, value: 'x', fun: () => { } },
-        { key: 3, value: '/', fun: () => { } },
-        { key: 5, value: '7', fun: handleDigitClick },
-        { key: 6, value: '8', fun: handleDigitClick },
-        { key: 7, value: '9', fun: handleDigitClick },
-        { key: 8, value: '=', fun: () => { } },
-        { key: 9, value: '4', fun: handleDigitClick },
-        { key: 10, value: '5', fun: handleDigitClick },
-        { key: 11, value: '6', fun: handleDigitClick },
-        { key: 12, value: '1', fun: handleDigitClick },
-        { key: 13, value: '2', fun: handleDigitClick },
-        { key: 14, value: '3', fun: handleDigitClick },
-        { key: 15, value: '0', fun: handleDigitClick },
-        { key: 16, value: '.', fun: () => { } },
-        { key: 17, value: 'C', fun: () => setDisplayText('') }
+        { value: '+', fun: handleOperatorClick },
+        { value: '-', fun: handleOperatorClick },
+        { value: 'x', fun: handleOperatorClick },
+        { value: '/', fun: handleOperatorClick },
+        { value: '7', fun: handleDigitClick },
+        { value: '8', fun: handleDigitClick },
+        { value: '9', fun: handleDigitClick },
+        { value: '=', fun: () => { } },
+        { value: '4', fun: handleDigitClick },
+        { value: '5', fun: handleDigitClick },
+        { value: '6', fun: handleDigitClick },
+        { value: '1', fun: handleDigitClick },
+        { value: '2', fun: handleDigitClick },
+        { value: '3', fun: handleDigitClick },
+        { value: '0', fun: handleDigitClick },
+        { value: '.', fun: handleOperatorClick },
+        { value: 'C', fun: () => setDisplayText('') }
     ];
+    let keyCount = 0;
 
     return (
         <div className='calculator'>
             <div className='display'>{displayText}</div>
             <div className="keyboard">
                 {buttons.map(item => {
+                    keyCount += 1;
                     return (
-                        <div className="button" onClick={item.fun} value={item.value}>{item.value}</div>
+                        <div key={keyCount} className="button" onClick={item.fun} value={item.value}>{item.value}</div>
                     );
                 })}
             </div>
