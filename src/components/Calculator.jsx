f90ee85e0ea8be3568060ff773585d4ef3cf1b86
Alessio Manfredini <alessio.manfredini.work@gmail.com>
2024-04-30 09:30:37 +0200

    :lipstick: Add display

---
+++ src/components/Calculator.jsx
@@ -0,0 +1,11 @@
+import '../static/css/style.css'
+
+function Calculator(){
+    return(
+        <div className='calculator'>
+            <div className='display'>2+2+3</div>
+        </div>
+    );
+}
+
+export default Calculator;
